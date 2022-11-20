6e8295e9a51d0b335f5f16b59fc84003d49051e0
maciej88 <mkrywult88@gmail.com>
2022-11-20 20:09:14 +0100

    urls for rest

--- djangoapi/courses/urls.py
+++ djangoapi/courses/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseView)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
