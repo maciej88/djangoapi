3d39e2e366ee471254d48b01bfd00aa0e50789aa
maciej88 <mkrywult88@gmail.com>
2022-11-20 20:08:33 +0100

    viewset bug

--- djangoapi/courses/views.py
+++ djangoapi/courses/views.py
@@ -3,6 +3,6 @@ from rest_framework import viewsets
 from .models import Course
 from .serializers import CourseSerializer
 
-class CourseView(viewsets.ModelSerializer):
+class CourseView(viewsets.ModelViewSet):
 	queryset = Course.objects.all()
 	serializer_class = CourseSerializer
