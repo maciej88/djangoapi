babd54b8ab27790e80ad1898c17fbd716680300d
maciej88 <mkrywult88@gmail.com>
2022-11-20 19:59:21 +0100

    viewset in views.py

--- djangoapi/courses/views.py
+++ djangoapi/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course
+from .serializers import CourseSerializer
 
-# Create your views here.
+class CourseView(viewsets.ModelSerializer):
+	queryset = Course.objects.all()
+	serializer_class = CourseSerializer
